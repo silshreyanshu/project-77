2fe7d9d8192d4b1d4fddf6000cebbfa0ff5667ca
silshreyanshu <90265680+silshreyanshu@users.noreply.github.com>
2022-08-23 20:09:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("My first console project")
+var first_name = "Ranbir";
+var Last_name = "Kapoor";
+console.log(first_name.concat(Last_name));
+var a = 10;
+var b = 6;
+var add = a + b;
+console.log(add);
